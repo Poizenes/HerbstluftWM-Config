5eb3e72e7ec12fa466d59a72b43c4d2d0b9dadf1
Dominik Kaiser <dominik-kaiser@swisscows.email>
2023-07-19 12:52:58 +0200

    Change config

--- python/config.py
+++ python/config.py
@@ -1,6 +1,10 @@
 keybinds = {
         'Super-Return': 'spawn alacritty',
         'Super-d': 'spawn rofi -show drun',
+        'Super-h': 'split h',
+        'Super-r': 'close_and_remove',
+
+        'Super-Shift-c': 'cycle_frame',
         }
 
 kb_tag_use = 'Super-'
